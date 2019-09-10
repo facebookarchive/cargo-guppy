198349956fe9f964a6d194d06261ed30d7ad7cee
Rain <rain1@calibra.com>
2019-09-10 13:25:33 -0700

    graph: refactor graph building logic

--- src/graph.rs
+++ src/graph.rs
@@ -1,18 +1,26 @@
 use crate::errors::Error;
-use cargo_metadata::{Metadata, MetadataCommand, NodeDep, Package, PackageId};
+use cargo_metadata::{Metadata, MetadataCommand, NodeDep, Package, PackageId, Resolve};
 use petgraph::prelude::*;
+use semver::Version;
 use std::collections::{HashMap, HashSet};
 
 pub struct PackageGraph {
-    packages: HashMap<PackageId, PackageData>,
+    packages: HashMap<PackageId, PackageMetadata>,
     dep_graph: Graph<PackageId, ()>,
 }
 
 #[derive(Clone, Debug)]
-pub struct PackageData {
+pub struct PackageMetadata {
+    // Fields extracted from the package.
+    name: String,
+    version: Version,
+    authors: Vec<String>,
+    description: Option<String>,
+    license: Option<String>,
+
+    // Other information.
     node_idx: NodeIndex<u32>,
     in_workspace: bool,
-    package: Package,
     resolved_deps: Vec<NodeDep>,
     resolved_features: Vec<String>,
 }
@@ -29,50 +37,17 @@ impl PackageGraph {
             )
         })?;
 
-        let mut resolve_data: HashMap<_, _> = resolve
-            .nodes
-            .into_iter()
-            .map(|node| (node.id, (node.deps, node.features)))
-            .collect();
-
-        let workspace_members = metadata
-            .workspace_members
-            .into_iter()
-            .collect::<HashSet<_>>();
-
-        let mut dep_graph: Graph<_, ()> = Graph::new();
+        let mut build_state = GraphBuildState::new(resolve, metadata.workspace_members);
 
         let packages: HashMap<_, _> = metadata
             .packages
             .into_iter()
-            .map(|package| {
-                let node_idx = dep_graph.add_node(package.id.clone());
-                let in_workspace = workspace_members.contains(&package.id);
-                let (resolved_deps, resolved_features) = match resolve_data.remove(&package.id) {
-                    Some(resolve_data) => resolve_data,
-                    None => {
-                        return Err(Error::DepGraphError(format!(
-                            "no resolve data found for package '{}'",
-                            package.id
-                        )));
-                    }
-                };
-                Ok((
-                    package.id.clone(),
-                    PackageData {
-                        node_idx,
-                        in_workspace,
-                        package,
-                        resolved_deps,
-                        resolved_features,
-                    },
-                ))
-            })
+            .map(|package| build_state.add_package(package))
             .collect::<Result<_, _>>()?;
 
+        let mut dep_graph = build_state.finish();
+
         for (id, data) in &packages {
-            // TODO: use the resolved deps to figure out what deps are being used
-            // see https://github.com/sfackler/cargo-tree/blob/master/src/main.rs#L388
             for dep in &data.resolved_deps {
                 let dep_id = &dep.pkg;
                 let dep_data = packages.get(dep_id).ok_or_else(|| {
@@ -81,6 +56,7 @@ impl PackageGraph {
                         id, dep_id
                     ))
                 })?;
+
                 dep_graph.add_edge(data.node_idx, dep_data.node_idx, ());
             }
         }
@@ -89,5 +65,63 @@ impl PackageGraph {
             packages,
             dep_graph,
         })
+    }
+}
+
+// Helper struct for building up graph nodes (not edges, those are done afterwards)
+struct GraphBuildState {
+    dep_graph: Graph<PackageId, ()>,
+    resolve_data: HashMap<PackageId, (Vec<NodeDep>, Vec<String>)>,
+    workspace_members: HashSet<PackageId>,
+}
+
+impl GraphBuildState {
+    fn new(resolve: Resolve, workspace_members: Vec<PackageId>) -> Self {
+        let dep_graph = Graph::new();
+
+        let resolve_data: HashMap<_, _> = resolve
+            .nodes
+            .into_iter()
+            .map(|node| (node.id, (node.deps, node.features)))
+            .collect();
+
+        let workspace_members = workspace_members.into_iter().collect::<HashSet<_>>();
+
+        Self {
+            dep_graph,
+            workspace_members,
+            resolve_data,
+        }
+    }
+
+    fn add_package(&mut self, package: Package) -> Result<(PackageId, PackageMetadata), Error> {
+        let node_idx = self.dep_graph.add_node(package.id.clone());
+        let in_workspace = self.workspace_members.contains(&package.id);
+        let (resolved_deps, resolved_features) =
+            self.resolve_data.remove(&package.id).ok_or_else(|| {
+                Error::DepGraphError(format!(
+                    "no resolve data found for package '{}'",
+                    package.id
+                ))
+            })?;
+        Ok((
+            package.id,
+            PackageMetadata {
+                name: package.name,
+                version: package.version,
+                authors: package.authors,
+                description: package.description,
+                license: package.license,
+
+                node_idx,
+                in_workspace,
+                resolved_deps,
+                resolved_features,
+            },
+        ))
+    }
+
+    fn finish(self) -> Graph<PackageId, ()> {
+        self.dep_graph
     }
 }
