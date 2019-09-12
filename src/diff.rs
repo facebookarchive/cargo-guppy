1b34f6eb556d3b1fc63a66c2d1a2cd1c832b3c02
Brandon Williams <bmwill@fb.com>
2019-09-12 08:32:56 -0700

    diff: display remaining versions for removed packages

--- src/diff.rs
+++ src/diff.rs
@@ -2,7 +2,6 @@ use crate::Lockfile;
 use crate::PackageId;
 use serde::Serialize;
 use std::collections::HashMap;
-use std::collections::HashSet;
 
 #[derive(Debug, Default)]
 pub struct DiffOptions;
@@ -11,12 +10,33 @@ impl DiffOptions {
     pub fn diff(&self, old_lockfile: &Lockfile, new_lockfile: &Lockfile) -> Diff {
         let mut new = new_lockfile.packages().clone();
 
-        let mut removed = HashSet::new();
-        for (pkg_id, _pkg) in old_lockfile.packages() {
-            if new.remove(pkg_id).is_none() {
-                removed.insert(pkg_id.clone());
-            }
-        }
+        let mut removed = old_lockfile
+            .packages()
+            .iter()
+            .filter_map(|(pkg_id, _pkg)| {
+                if new.remove(pkg_id).is_none() {
+                    Some(pkg_id.clone())
+                } else {
+                    None
+                }
+            })
+            .map(|removed_pkg_id| {
+                let remaining_packages = new_lockfile
+                    .packages()
+                    .iter()
+                    .filter(|(pkg_id, _)| {
+                        (**pkg_id != removed_pkg_id) && (pkg_id.name() == removed_pkg_id.name())
+                    })
+                    .map(|(pkg_id, _)| pkg_id.clone())
+                    .collect::<Vec<_>>();
+
+                if remaining_packages.is_empty() {
+                    (removed_pkg_id.clone(), None)
+                } else {
+                    (removed_pkg_id.clone(), Some(remaining_packages))
+                }
+            })
+            .collect::<HashMap<_, _>>();
 
         let mut added = new
             .into_iter()
@@ -40,7 +60,7 @@ impl DiffOptions {
 
         let mut updated = removed
             .iter()
-            .filter_map(|removed_pkg_id| {
+            .filter_map(|(removed_pkg_id, _)| {
                 if let Some((updated, _)) = added
                     .iter()
                     .find(|added_pkg| removed_pkg_id.name() == added_pkg.0.name())
@@ -59,6 +79,8 @@ impl DiffOptions {
             added.remove(added_pkg_id);
         }
 
+        let mut removed = removed.into_iter().collect::<Vec<_>>();
+        removed.sort_by(|(a, _), (b, _)| a.name().cmp(b.name()));
         let mut added = added.into_iter().collect::<Vec<_>>();
         added.sort_by(|(a, _), (b, _)| a.name().cmp(b.name()));
 
@@ -73,12 +95,27 @@ impl DiffOptions {
 #[derive(Debug, Serialize)]
 pub struct Diff {
     updated: Vec<(PackageId, PackageId)>,
-    removed: HashSet<PackageId>,
+    removed: Vec<(PackageId, Option<Vec<PackageId>>)>,
     added: Vec<(PackageId, Option<Vec<PackageId>>)>,
 }
 
 impl ::std::fmt::Display for Diff {
     fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
+        fn write_dups(
+            f: &mut ::std::fmt::Formatter<'_>,
+            dups: &Option<Vec<PackageId>>,
+        ) -> ::std::fmt::Result {
+            if let Some(dups) = dups {
+                write!(f, " ({}", dups[0].version())?;
+                for p in &dups[1..] {
+                    write!(f, ", {}", p.version())?;
+                }
+                write!(f, ")")?;
+            }
+
+            Ok(())
+        }
+
         if !self.updated.is_empty() {
             writeln!(f, "Updated Packages:")?;
             for (removed, added) in &self.updated {
@@ -94,27 +131,22 @@ impl ::std::fmt::Display for Diff {
         }
 
         if !self.removed.is_empty() {
-            writeln!(f, "Removed Packages:")?;
-            let mut removed_sorted = self.removed.iter().collect::<Vec<_>>();
-            removed_sorted.sort_by(|a, b| a.name().cmp(b.name()));
-            for removed in removed_sorted {
-                writeln!(f, "\t{} {}", removed.name(), removed.version(),)?;
+            writeln!(f, "Removed Packages (Remaining versions in '()'):")?;
+            for (removed, dups) in &self.removed {
+                write!(f, "\t{} {}", removed.name(), removed.version(),)?;
+
+                write_dups(f, dups)?;
+                writeln!(f)?;
             }
             writeln!(f)?;
         }
 
         if !self.added.is_empty() {
-            writeln!(f, "Added Packages (existing duplicates in '()'):")?;
+            writeln!(f, "Added Packages (Duplicate versions in '()'):")?;
             for (added, dups) in &self.added {
                 write!(f, "\t{} {}", added.name(), added.version(),)?;
 
-                if let Some(dups) = dups {
-                    write!(f, " ({}", dups[0].version())?;
-                    for p in &dups[1..] {
-                        write!(f, ", {}", p.version())?;
-                    }
-                    write!(f, ")")?;
-                }
+                write_dups(f, dups)?;
                 writeln!(f)?;
             }
             writeln!(f)?;
