dcba433cdde6e9ca703ef8ab5c1159d438280176
Rain <rain1@calibra.com>
2019-09-19 14:30:32 -0700

    graph: refactor graph logic into separate module

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,7 @@
 mod config;
 pub mod diff;
 mod errors;
-pub mod graph_build;
-mod graph_walk;
+pub mod graph;
 pub mod lockfile;
 #[cfg(test)]
 mod unit_tests;
--- src/unit_tests/fixtures.rs
+++ src/unit_tests/fixtures.rs
@@ -1,7 +1,7 @@
 // Copyright (c) The cargo-guppy Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use crate::graph_build::{PackageDep, PackageGraph, PackageMetadata, Workspace};
+use crate::graph::{PackageDep, PackageGraph, PackageMetadata, Workspace};
 use cargo_metadata::PackageId;
 use semver::Version;
 use std::collections::{BTreeMap, HashMap};
