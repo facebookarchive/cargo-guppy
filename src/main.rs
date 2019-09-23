797eca332da6bb939a5fffb5c221d6124d8525fd
Brandon Williams <bmwill@fb.com>
2019-09-23 15:00:20 -0700

    diff: working Cargo.lock difftool

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Cargo.lock file analysis")]
+struct Args {
+    #[structopt(subcommand)]
+    cmd: Command,
+}
+
+#[derive(Debug, StructOpt)]
+enum Command {
+    #[structopt(name = "diff")]
+    /// Perform a diff of two Cargo.lock files
+    Diff { old: String, new: String },
+}
+
+fn main() {
+    let args = Args::from_args();
+
+    let result = match args.cmd {
+        Command::Diff { old, new } => cargo_guppy::cmd_diff(&old, &new),
+    };
+
+    match result {
+        Err(e) => println!("{}\nAborting...", e),
+        Ok(()) => {}
+    }
+}
