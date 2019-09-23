dde15fcd9b16e096c5e17f51fd790ae08e8f6c39
Brandon Williams <bmwill@calibra.com>
2019-09-23 14:27:01 -0700

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+// Copyright (c) The cargo-guppy Contributors
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
