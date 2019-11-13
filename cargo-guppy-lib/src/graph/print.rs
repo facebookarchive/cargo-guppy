4a92d145af8245f916c0fe47a936fd4eabd5a9a4
Rain <rain1@fb.com>
2019-11-13 15:48:37 -0800

    [graph] add a way to generate dot graphs for package graphs and subgraphs
    This can be useful for visually inspecting package graphs.
    
    This is not the final API, I'm pretty sure. I have ideas on how to structure this as a query language.

---
+++ cargo-guppy-lib/src/graph/print.rs
@@ -0,0 +1,111 @@
+use crate::errors::*;
+use crate::graph::visit::dot::{DotFmt, DotVisitor, DotWrite};
+use crate::graph::visit::reversed::{ReverseFlip, ReversedDirected};
+use crate::graph::{DependencyEdge, DependencyLink, PackageGraph, PackageMetadata};
+use cargo_metadata::PackageId;
+use petgraph::prelude::*;
+use petgraph::visit::{IntoNeighbors, NodeFiltered, NodeRef, Visitable};
+use std::fmt;
+
+/// A visitor used for formatting `dot` graphs.
+pub trait PackageDotVisitor {
+    /// Visits this package. The implementation may output a label for this package to the given
+    /// `DotWrite`.
+    fn visit_package(&self, package: &PackageMetadata, f: DotWrite<'_, '_>) -> fmt::Result;
+
+    /// Visits this dependency link. The implementation may output a label for this link to the
+    /// given `DotWrite`.
+    fn visit_link(&self, link: DependencyLink<'_>, f: DotWrite<'_, '_>) -> fmt::Result;
+}
+
+impl PackageGraph {
+    /// Constructs a representation of this graph in `dot` format.
+    pub fn make_dot<'g, V>(&'g self, visitor: V) -> impl fmt::Display + 'g
+    where
+        V: 'g + PackageDotVisitor,
+    {
+        DotFmt::new(&self.dep_graph, VisitorWrap::new(self, visitor))
+    }
+
+    /// Constructs a representation of all packages that are dependencies of the given roots, in
+    /// `dot` format.
+    pub fn make_dot_reachable<'g, 'a, V>(
+        &'g self,
+        roots: impl IntoIterator<Item = &'a PackageId>,
+        visitor: V,
+    ) -> Result<impl fmt::Display + 'g, Error>
+    where
+        V: 'g + PackageDotVisitor,
+    {
+        let node_idxs = self.node_idxs(roots)?;
+        let node_filtered =
+            NodeFiltered(&self.dep_graph, reachable_map(&self.dep_graph, node_idxs));
+        Ok(DotFmt::new(node_filtered, VisitorWrap::new(self, visitor)))
+    }
+
+    /// Constructs a representation of all packages that are dependents of the given roots, in
+    /// `dot` format.
+    pub fn make_dot_reachable_reversed<'g, 'a, V>(
+        &'g self,
+        roots: impl IntoIterator<Item = &'a PackageId>,
+        visitor: V,
+    ) -> Result<impl fmt::Display + 'g, Error>
+    where
+        V: 'g + PackageDotVisitor,
+    {
+        let node_idxs = self.node_idxs(roots)?;
+        // The reachable_map is computed over the reversed graph, while the actual iteration happens
+        // over the regular graph (so that arrows in the graph are in the right direction).
+        let node_filtered = NodeFiltered(
+            &self.dep_graph,
+            reachable_map(ReversedDirected::new(&self.dep_graph), node_idxs),
+        );
+        Ok(DotFmt::new(node_filtered, VisitorWrap::new(self, visitor)))
+    }
+}
+
+struct VisitorWrap<'g, V> {
+    graph: &'g PackageGraph,
+    inner: V,
+}
+
+impl<'g, V> VisitorWrap<'g, V> {
+    fn new(graph: &'g PackageGraph, inner: V) -> Self {
+        Self { graph, inner }
+    }
+}
+
+impl<'g, V, NR, ER> DotVisitor<NR, ER> for VisitorWrap<'g, V>
+where
+    V: PackageDotVisitor,
+    NR: NodeRef<NodeId = NodeIndex<u32>, Weight = PackageId>,
+    ER: EdgeRef<NodeId = NodeIndex<u32>, Weight = DependencyEdge> + ReverseFlip,
+{
+    fn visit_node(&self, node: NR, f: DotWrite<'_, '_>) -> fmt::Result {
+        let metadata = self
+            .graph
+            .metadata(node.weight())
+            .expect("visited node should have associated metadata");
+        self.inner.visit_package(metadata, f)
+    }
+
+    fn visit_edge(&self, edge: ER, f: DotWrite<'_, '_>) -> fmt::Result {
+        let (source_idx, target_idx) = ER::reverse_flip(edge.source(), edge.target());
+        let link = self
+            .graph
+            .edge_to_link(source_idx, target_idx, edge.weight());
+        self.inner.visit_link(link, f)
+    }
+}
+
+fn reachable_map<G>(graph: G, roots: Vec<G::NodeId>) -> G::Map
+where
+    G: Visitable + IntoNeighbors,
+{
+    // To figure out what nodes are reachable, run a DFS starting from the roots.
+    let mut dfs = Dfs::from_parts(roots, graph.visit_map());
+    while let Some(_) = dfs.next(graph) {}
+
+    // Once the DFS is done, the discovered map is what's allowed.
+    dfs.discovered
+}
--- cargo-guppy-lib/src/unit_tests/graph_tests.rs
+++ cargo-guppy-lib/src/unit_tests/graph_tests.rs
@@ -1,5 +1,8 @@
 use super::fixtures::{self, Fixture};
+use crate::graph::{DependencyLink, DotWrite, PackageDotVisitor, PackageMetadata};
 use cargo_metadata::PackageId;
+use std::fmt::{self, Write};
+use std::iter;
 
 // Test specific details extracted from metadata1.json.
 #[test]
@@ -21,6 +24,56 @@ fn metadata1() {
     assert!(dep.edge.normal().is_some(), "normal dependency is defined");
     assert!(dep.edge.build().is_some(), "build dependency is defined");
     assert!(dep.edge.dev().is_some(), "dev dependency is defined");
+
+    // Print out dot graphs for small subgraphs.
+    static EXPECTED_DOT: &str = r#"digraph {
+    0 [label="winapi-x86_64-pc-windows-gnu"]
+    11 [label="mach"]
+    13 [label="winapi"]
+    14 [label="libc"]
+    20 [label="winapi-i686-pc-windows-gnu"]
+    31 [label="bitflags"]
+    11 -> 14 [label="libc"]
+    13 -> 20 [label="winapi-i686-pc-windows-gnu"]
+    13 -> 0 [label="winapi-x86_64-pc-windows-gnu"]
+}
+"#;
+    assert_eq!(
+        EXPECTED_DOT,
+        format!(
+            "{}",
+            graph
+                .make_dot_reachable(
+                    iter::once(&fixtures::package_id(fixtures::METADATA1_REGION)),
+                    NameVisitor
+                )
+                .unwrap()
+        ),
+        "dot output matches"
+    );
+
+    // For reverse reachable ensure that the arrows are in the correct direction.
+    static EXPECTED_DOT_REVERSED: &str = r#"digraph {
+    1 [label="datatest"]
+    9 [label="serde_yaml"]
+    18 [label="testcrate"]
+    1 -> 9 [label="serde_yaml"]
+    18 -> 1 [label="datatest"]
+}
+"#;
+    assert_eq!(
+        EXPECTED_DOT_REVERSED,
+        format!(
+            "{}",
+            graph
+                .make_dot_reachable_reversed(
+                    iter::once(&fixtures::package_id(fixtures::METADATA1_DTOA)),
+                    NameVisitor
+                )
+                .unwrap()
+        ),
+        "reversed dot output matches"
+    );
 }
 
 #[test]
@@ -33,4 +86,24 @@ fn metadata2() {
 fn metadata_libra() {
     let metadata_libra = Fixture::metadata_libra();
     metadata_libra.verify();
+
+    let graph = metadata_libra.graph();
+    println!(
+        "{}",
+        graph
+            .make_dot_reachable_reversed(graph.workspace().members().map(|(_, id)| id), NameVisitor)
+            .unwrap()
+    );
+}
+
+struct NameVisitor;
+
+impl PackageDotVisitor for NameVisitor {
+    fn visit_package(&self, package: &PackageMetadata, mut f: DotWrite<'_, '_>) -> fmt::Result {
+        write!(f, "{}", package.name())
+    }
+
+    fn visit_link(&self, link: DependencyLink<'_>, mut f: DotWrite<'_, '_>) -> fmt::Result {
+        write!(f, "{}", link.edge.dep_name())
+    }
 }
