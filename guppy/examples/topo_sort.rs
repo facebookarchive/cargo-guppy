4caba2082e98e9916556df924b464b87e0b32176
Rain <rain1@fb.com>
2019-11-20 15:36:14 -0800

    [examples] add a topo sort example
    Topo sorts are cool, so add an example showing how to make one happen.

---
+++ guppy/examples/topo_sort.rs
@@ -0,0 +1,30 @@
+//! Print out crates in a workspace in topological order.
+//!
+//! The into_iter_ids and into_iter_metadatas iterators return packages in topological order. Note
+//! that into_iter_links returns links in "link order" -- see its documentation for more.
+
+use guppy::graph::{DependencyDirection, PackageGraph};
+use guppy::Error;
+
+fn main() -> Result<(), Error> {
+    // `guppy` accepts `cargo metadata` JSON output. Use a pre-existing fixture for these examples.
+    let fixture = include_str!("../fixtures/metadata_libra.json");
+    let package_graph = PackageGraph::from_json(fixture)?;
+
+    // Non-workspace packages cannot depend on packages within the workspace, so the reverse
+    // transitive deps of workspace packages are exactly the set of workspace packages.
+    let select =
+        package_graph.select_transitive_reverse_deps(package_graph.workspace().member_ids())?;
+
+    // Here we pass in `Forward` -- by default, select_transitive_reverse_deps will cause packages
+    // to be returned in reverse topo order.
+    for package in select.into_iter_metadatas(Some(DependencyDirection::Forward)) {
+        // All selected packages are in the workspace.
+        let workspace_path = package
+            .workspace_path()
+            .expect("packages in workspace should have workspace path");
+        println!("{}: {:?}", package.name(), workspace_path);
+    }
+
+    Ok(())
+}
