58bdf591e1d2ead1065bad7f681ee9d92988b737
Rain <rain1@fb.com>
2019-11-20 14:18:03 -0800

    [guppy] move example snippets into examples dir
    This means users can easily run these examples with `cargo run --example foo`.

---
+++ guppy/examples/deps.rs
@@ -0,0 +1,34 @@
+//! Print out direct and transitive dependencies of a package.
+
+use cargo_metadata::PackageId;
+use guppy::graph::PackageGraph;
+use guppy::Error;
+use std::iter;
+
+fn main() -> Result<(), Error> {
+    // `guppy` accepts `cargo metadata` JSON output. Use a pre-existing fixture for these examples.
+    let fixture = include_str!("../fixtures/metadata1.json");
+    let package_graph = PackageGraph::from_json(fixture)?;
+
+    // `guppy` provides several ways to get hold of package IDs. Use a pre-defined one for this
+    // example.
+    let package_id = PackageId {
+        repr: "testcrate 0.1.0 (path+file:///fakepath/testcrate)".into(),
+    };
+    // dep_links returns all direct dependencies of a package, and it returns `None` if the package
+    // ID isn't recognized.
+    for link in package_graph.dep_links(&package_id).unwrap() {
+        // A dependency link contains `from`, `to` and `edge`. The edge has information about e.g.
+        // whether this is a build dependency.
+        println!("direct: {}", link.to.id());
+    }
+
+    // Transitive dependencies are obtained through the `select_` APIs. They are always presented in
+    // topological order.
+    let select = package_graph.select_transitive_deps(iter::once(&package_id))?;
+    for dep_id in select.into_iter_ids(None) {
+        // The select API also has an `into_iter_links()` method which returns links instead of IDs.
+        println!("transitive: {}", dep_id);
+    }
+    Ok(())
+}
---
+++ guppy/examples/remove_dev_only.rs
@@ -0,0 +1,62 @@
+//! Remove all dependency links that are dev-only.
+//!
+//! Dev-only dependencies are typically not included in release builds, so it's useful to be able
+//! to filter out those links.
+
+use guppy::graph::PackageGraph;
+use guppy::Error;
+use std::iter;
+
+fn main() -> Result<(), Error> {
+    // `guppy` accepts `cargo metadata` JSON output. Use a pre-existing fixture for these examples.
+    let fixture = include_str!("../fixtures/metadata_libra.json");
+    let mut package_graph = PackageGraph::from_json(fixture)?;
+
+    // Pick an important binary package and compute the number of dependencies.
+    //
+    // A clone is typically not required but in this case we're mutating the graph, so we need to
+    // release the immutatable borrow.
+    let libra_node_id = package_graph
+        .workspace()
+        .member_by_path("libra-node")
+        .unwrap()
+        .clone();
+
+    let before_count = package_graph
+        .select_transitive_deps(iter::once(&libra_node_id))?
+        .into_iter_ids(None)
+        .count();
+    println!("number of packages before: {}", before_count);
+
+    // `retain_edges` takes a closure that returns `true` if this edge should be kept in the graph.
+    package_graph.retain_edges(|_data, link| {
+        // '_data' contains metadata for every package. It isn't used in this example but some
+        // complex filters may make use of it.
+
+        if link.edge.dev_only() {
+            println!(
+                "filtering out dev-only link: {} -> {}",
+                link.from.name(),
+                link.to.name()
+            );
+            return false;
+        }
+        true
+    });
+
+    // Iterate over all links and assert that there are no dev-only links.
+    for link in package_graph.select_all().into_iter_links(None) {
+        if !link.from.in_workspace() || !link.to.in_workspace() {
+            assert!(!link.edge.dev_only());
+        }
+    }
+
+    // Count the number of packages after.
+    let after_count = package_graph
+        .select_transitive_deps(iter::once(&libra_node_id))?
+        .into_iter_ids(None)
+        .count();
+    println!("number of packages after: {}", after_count);
+
+    Ok(())
+}
