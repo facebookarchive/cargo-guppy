debd4c58fde30c5fd497257639f6f4d8b8bb4e01
ci-build <ci-build@calibra.com>
2019-12-29 21:23:55 +0000

    [skip ci] documentation update

--- implementors/structopt/trait.StructOpt.js
+++ implementors/structopt/trait.StructOpt.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors["cargo_guppy"] = [{text:"impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"cargo_guppy/struct.SelectOptions.html\" title=\"struct cargo_guppy::SelectOptions\">SelectOptions</a>",synthetic:false,types:["cargo_guppy::SelectOptions"]},];
+implementors["structopt"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
