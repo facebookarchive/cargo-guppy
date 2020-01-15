b447ae69317c2fb9737079e4a0ede8ee7d06b1fe
Jack Moffitt <metajack@fb.com>
2020-01-14 21:43:07 -0800

    [lockfile] Remove unused lockfile code

--- lockfile/README.md
+++
@@ -1,3 +0,0 @@
-# lockfile
-
-This crate contains earlier work for parsing lockfiles. It is here temporarily and will eventually be removed.
--- lockfile/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) The cargo-guppy Contributors
-// SPDX-License-Identifier: MIT OR Apache-2.0
-
-mod errors;
-pub mod lockfile;
-
-pub use errors::Error;
