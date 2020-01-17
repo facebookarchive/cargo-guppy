57b4923942b49323a78f7c662af56a0d04c32c93
ci-build <ci-build@calibra.com>
2020-01-17 22:53:23 +0000

    [skip ci] documentation update

--- cargo_guppy/sidebar-items.js
+++ cargo_guppy/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Kind",""]],"fn":[["cmd_count",""],["cmd_diff",""],["cmd_dups",""],["cmd_select",""]],"struct":[["SelectOptions",""]]});
+initSidebarItems({"enum":[["Kind",""]],"fn":[["cmd_count",""],["cmd_diff",""],["cmd_dups",""],["cmd_select",""],["cmd_subtree_size",""]],"struct":[["SelectOptions",""],["SubtreeSizeOptions",""]]});
--- implementors/structopt/trait.StructOpt.js
+++ implementors/structopt/trait.StructOpt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["cargo_guppy"] = [{text:"impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"cargo_guppy/struct.SelectOptions.html\" title=\"struct cargo_guppy::SelectOptions\">SelectOptions</a>",synthetic:false,types:["cargo_guppy::SelectOptions"]},];
+implementors["cargo_guppy"] = [{text:"impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"cargo_guppy/struct.SelectOptions.html\" title=\"struct cargo_guppy::SelectOptions\">SelectOptions</a>",synthetic:false,types:["cargo_guppy::SelectOptions"]},{text:"impl <a class=\"trait\" href=\"structopt/trait.StructOpt.html\" title=\"trait structopt::StructOpt\">StructOpt</a> for <a class=\"struct\" href=\"cargo_guppy/struct.SubtreeSizeOptions.html\" title=\"struct cargo_guppy::SubtreeSizeOptions\">SubtreeSizeOptions</a>",synthetic:false,types:["cargo_guppy::SubtreeSizeOptions"]},];
 implementors["structopt"] = [];
 
             if (window.register_implementors) {
