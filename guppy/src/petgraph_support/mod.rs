21d8b1ae3686d468f6c867fe91928a8e8eac0f1d
Rain <rain1@fb.com>
2020-01-22 13:23:36 -0800

    [select] make some code more generic
    Much of this code will be reused for feature graphs.

--- guppy/src/petgraph_support/mod.rs
+++ guppy/src/petgraph_support/mod.rs
@@ -6,7 +6,10 @@
 //! The code in here is generic over petgraph's traits, and could be upstreamed into petgraph if
 //! desirable.
 
-use petgraph::visit::EdgeRef;
+use petgraph::graph::IndexType;
+use petgraph::prelude::*;
+use petgraph::visit::{IntoNeighborsDirected, IntoNodeIdentifiers};
+use std::iter;
 
 pub mod dot;
 pub mod reversed;
@@ -14,4 +17,17 @@ pub mod walk;
 
 pub fn edge_triple<ER: EdgeRef>(edge_ref: ER) -> (ER::NodeId, ER::NodeId, ER::EdgeId) {
     (edge_ref.source(), edge_ref.target(), edge_ref.id())
+}
+
+/// Returns the nodes of a graph that have no incoming edges to them.
+pub fn externals<G, Ix, B>(graph: G) -> B
+where
+    G: IntoNodeIdentifiers + IntoNeighborsDirected<NodeId = NodeIndex<Ix>>,
+    Ix: IndexType,
+    B: iter::FromIterator<NodeIndex<Ix>>,
+{
+    graph
+        .node_identifiers()
+        .filter(move |&a| graph.neighbors_directed(a, Incoming).next().is_none())
+        .collect()
 }
