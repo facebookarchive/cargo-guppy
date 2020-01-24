b434fd60bbd7e5c668560eb6c3a31eb6e3ba7094
Rain <rain1@fb.com>
2020-01-24 14:11:16 -0800

    [select] fix into_root_ids
    Previously we'd just return the input set in some cases, even though it
    might have contained packages that were dependent on other ones.

--- guppy/src/unit_tests/proptest_helpers.rs
+++ guppy/src/unit_tests/proptest_helpers.rs
@@ -2,9 +2,13 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 use crate::graph::{DependencyDirection, PackageGraph};
-use crate::unit_tests::dep_helpers::{assert_depends_on_any, assert_link_order};
+use crate::unit_tests::dep_helpers::{
+    assert_depends_on_any, assert_link_order, assert_not_depends_on,
+};
 use crate::PackageId;
+use proptest09::prelude::*;
 use proptest09::sample::Index;
+use std::collections::HashSet;
 
 macro_rules! proptest_suite {
     ($name: ident) => {
@@ -42,6 +46,20 @@ macro_rules! proptest_suite {
                     query_direction in any::<DependencyDirection>(),
                 )| {
                     link_order(graph, &ids, select_direction, query_direction, "select_link_order");
+                });
+            }
+
+            #[test]
+            fn proptest_select_roots() {
+                let fixture = Fixture::$name();
+                let graph = fixture.graph();
+
+                proptest!(|(
+                    ids in vec(graph.prop09_id_strategy(), 1..16),
+                    select_direction in any::<DependencyDirection>(),
+                    query_direction in any::<DependencyDirection>(),
+                )| {
+                    roots(graph, &ids, select_direction, query_direction, "select_roots")?;
                 });
             }
         }
@@ -99,4 +117,48 @@ pub(crate) fn link_order(
         query_direction,
         &format!("{}: link order", msg),
     );
+}
+
+/// Test that the results of an `into_root_ids` query don't depend on any other root.
+pub(crate) fn roots(
+    graph: &PackageGraph,
+    ids: &[&PackageId],
+    select_direction: DependencyDirection,
+    query_direction: DependencyDirection,
+    msg: &str,
+) -> prop::test_runner::TestCaseResult {
+    let select = graph
+        .select_directed(ids.iter().copied(), select_direction)
+        .unwrap();
+    let root_ids: Vec<_> = select.clone().into_root_ids(query_direction).collect();
+    let root_metadatas: Vec<_> = select
+        .clone()
+        .into_root_metadatas(query_direction)
+        .collect();
+
+    // Check that root_ids and root_metadatas return the same number of elements.
+    assert_eq!(
+        root_ids.len(),
+        root_metadatas.len(),
+        "{}: root ids and root metadatas should have the same number of results",
+        msg
+    );
+
+    let root_id_set: HashSet<_> = root_ids.iter().collect();
+    assert_eq!(
+        root_ids.len(),
+        root_id_set.len(),
+        "{}: root IDs should all be unique",
+        msg
+    );
+
+    let mut cache = graph.new_depends_cache();
+    for id1 in &root_ids {
+        for id2 in &root_ids {
+            if id1 != id2 {
+                assert_not_depends_on(id1, id2, &mut cache, select_direction, msg);
+            }
+        }
+    }
+    Ok(())
 }
