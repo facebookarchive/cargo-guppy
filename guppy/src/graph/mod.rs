3c98b9ba4983428a9cc6218de360ee0283472f01
Rain <rain1@fb.com>
2020-01-24 13:53:28 -0800

    [graph] hide the feature module from documentation temporarily
    Want to do a release without this exposed, then later do a release with it
    once more functionality has been added.

--- guppy/src/graph/mod.rs
+++ guppy/src/graph/mod.rs
@@ -11,6 +11,8 @@ use petgraph::prelude::*;
 use std::fmt;
 
 mod build;
+// Temporarily hidden -- will be enabled once more feature stuff is added.
+#[doc(hidden)]
 pub mod feature;
 mod graph_impl;
 mod print;
