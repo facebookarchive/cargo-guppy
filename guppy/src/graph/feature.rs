bcb5c0458d9d0441937d6005936811048c650760
Rain <rain1@fb.com>
2020-01-23 17:08:22 -0800

    [graph] move FeatureGraph to its own module
    We're going to add a number of structs and functions to this module, so it's
    convenient to not include it as part of the main `graph` module.

--- guppy/src/graph/feature.rs
+++ guppy/src/graph/feature.rs
@@ -1,6 +1,12 @@
 // Copyright (c) The cargo-guppy Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+//! Graph analysis for individual features within a package.
+//!
+//! `FeatureGraph` can be used to do a more precise analysis than is possible at the package level.
+//! For example, an optional feature not included a default build can potentially pull in a large
+//! number of extra dependencies. This module allows for those subgraphs to be filtered out.
+
 use crate::errors::{FeatureBuildStage, FeatureGraphWarning};
 use crate::graph::{
     DependencyDirection, DependencyLink, FeatureIx, PackageGraph, PackageIx, PackageMetadata,
