5b0c43828e2ebd931211d060f74a1ac91735d381
Rain <rain1@fb.com>
2020-01-27 16:36:02 -0800

    [benchmarks] add an initial benchmark for depends_on with and without the cache
    The cache is a pretty significant speedup:
    
    depends_on              time:   [388.51 us 391.28 us 394.35 us]
    Found 5 outliers among 100 measurements (5.00%)
      2 (2.00%) low mild
      3 (3.00%) high mild
    
    depends_on_cache        time:   [303.76 us 313.56 us 324.69 us]
    Found 17 outliers among 100 measurements (17.00%)
      14 (14.00%) high mild
      3 (3.00%) high severe

---
+++ benchmarks/benches/package_graph.rs
@@ -0,0 +1,57 @@
+// Copyright (c) The cargo-guppy Contributors
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use guppy::graph::PackageGraph;
+use guppy::PackageId;
+use guppy_benchmarks::ValueGenerator;
+use proptest::collection::vec;
+use proptest::prelude::*;
+
+pub fn benchmarks(c: &mut Criterion) {
+    let package_graph = make_package_graph();
+    let mut cache = package_graph.new_depends_cache();
+    let mut gen = ValueGenerator::deterministic();
+
+    c.bench_function("depends_on", |b| {
+        b.iter_batched_ref(
+            || gen.generate(id_pairs_strategy(&package_graph)),
+            |package_ids| {
+                package_ids.iter().for_each(|(package_a, package_b)| {
+                    let _ = package_graph.depends_on(package_a, package_b);
+                })
+            },
+            BatchSize::SmallInput,
+        )
+    });
+
+    c.bench_function("depends_on_cache", |b| {
+        b.iter_batched_ref(
+            || gen.generate(id_pairs_strategy(&package_graph)),
+            |package_ids| {
+                package_ids.iter().for_each(|(package_a, package_b)| {
+                    let _ = cache.depends_on(package_a, package_b);
+                })
+            },
+            BatchSize::SmallInput,
+        )
+    });
+}
+
+fn make_package_graph() -> PackageGraph {
+    // Use this package graph as a large and representative one.
+    PackageGraph::from_json(include_str!("../../guppy/fixtures/metadata_libra.json")).unwrap()
+}
+
+/// Generate pairs of IDs for benchmarks.
+fn id_pairs_strategy<'g>(
+    graph: &'g PackageGraph,
+) -> impl Strategy<Value = Vec<(&'g PackageId, &'g PackageId)>> + 'g {
+    vec(
+        (graph.prop09_id_strategy(), graph.prop09_id_strategy()),
+        256,
+    )
+}
+
+criterion_group!(benches, benchmarks);
+criterion_main!(benches);
