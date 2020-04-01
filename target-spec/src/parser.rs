8a548e2d7eb8f883f4c4ea6545aec6608f06607a
Rain <rain1@fb.com>
2020-04-01 10:53:28 -0700

    [target-spec] rename TargetEnum back to Target
    Addressing review feedback -- it's somewhat inconvenient to try and amend
    the earlier commits with this change, so here it is.

--- target-spec/src/parser.rs
+++ target-spec/src/parser.rs
@@ -1,7 +1,7 @@
 // Copyright (c) The cargo-guppy Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use crate::TargetEnum;
+use crate::Target;
 use cfg_expr::targets::get_target_by_triple;
 use cfg_expr::Expression;
 use std::sync::Arc;
@@ -25,16 +25,15 @@ impl fmt::Display for ParseError {
 
 impl error::Error for ParseError {}
 
-/// Parses this expression into a `TargetEnum` instance.
-pub(crate) fn parse(input: &str) -> Result<TargetEnum, ParseError> {
+/// Parses this expression into a `Target` instance.
+pub(crate) fn parse(input: &str) -> Result<Target, ParseError> {
     if input.starts_with("cfg(") {
-        Ok(TargetEnum::Spec(Arc::new(
+        Ok(Target::Spec(Arc::new(
             Expression::parse(input).map_err(ParseError::new)?,
         )))
     } else {
-        Ok(TargetEnum::TargetInfo(
-            get_target_by_triple(input)
-                .ok_or_else(|| ParseError(format!("unrecognized target triple '{}'", input)))?,
-        ))
+        Ok(Target::TargetInfo(get_target_by_triple(input).ok_or_else(
+            || ParseError(format!("unrecognized target triple '{}'", input)),
+        )?))
     }
 }
