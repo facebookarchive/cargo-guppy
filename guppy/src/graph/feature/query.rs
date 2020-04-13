6930752113ca7ca09247fe0ceb9753db7985674f
Rain <rain1@fb.com>
2020-04-13 14:59:19 -0700

    [feature] simplify lifetimes a bit
    The latest nightly seems to complain about this, causing docs.rs to not be
    able to build guppy.

--- guppy/src/graph/feature/query.rs
+++ guppy/src/graph/feature/query.rs
@@ -178,12 +178,10 @@ impl<'g> FeatureGraph<'g> {
     ) -> FeatureQuery<'g> {
         let params = match &packages.params {
             QueryParams::Forward(package_ixs) => QueryParams::Forward(
-                self.feature_ixs_for_packages(package_ixs.iter().copied(), filter)
-                    .collect(),
+                self.feature_ixs_for_packages(package_ixs.iter().copied(), filter),
             ),
             QueryParams::Reverse(package_ixs) => QueryParams::Reverse(
-                self.feature_ixs_for_packages(package_ixs.iter().copied(), filter)
-                    .collect(),
+                self.feature_ixs_for_packages(package_ixs.iter().copied(), filter),
             ),
         };
 
