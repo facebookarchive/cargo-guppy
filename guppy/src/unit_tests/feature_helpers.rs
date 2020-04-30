7dba31a57b3ad7e0a6ca55a6653772587690dde7
Rain <rain1@fb.com>
2020-04-30 10:48:00 -0700

    [feature] add a wrapper struct for features of a single package
    This is somewhat messy to do with `Option<&'g str>`, so add a wrapper that
    handles all of it.

--- guppy/src/unit_tests/feature_helpers.rs
+++ guppy/src/unit_tests/feature_helpers.rs
@@ -1,9 +1,8 @@
 // Copyright (c) The cargo-guppy Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use crate::graph::feature::FeatureSet;
+use crate::graph::feature::{FeatureList, FeatureSet};
 use crate::PackageId;
-use std::collections::HashSet;
 
 pub(super) fn assert_features_for_package(
     feature_set: &FeatureSet<'_>,
@@ -11,11 +10,17 @@ pub(super) fn assert_features_for_package(
     expected: &[Option<&str>],
     msg: &str,
 ) {
-    let actual: HashSet<_> = feature_set
+    let actual = feature_set
         .features_for(package_id)
-        .expect("valid package ID")
-        .collect();
-    let expected: HashSet<_> = expected.iter().copied().collect();
+        .expect("valid package ID");
+    let expected = FeatureList::new(
+        feature_set
+            .graph()
+            .package_graph()
+            .metadata(package_id)
+            .expect("valid package ID"),
+        expected.iter().copied(),
+    );
 
     assert_eq!(
         actual, expected,
