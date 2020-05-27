b775b42540725c081e9a00e1d845e4a74d219471
ci-build <ci-build@calibra.com>
2020-05-27 23:18:55 +0000

    [skip ci] documentation update

---
+++ implementors/guppy/graph/cargo/trait.CargoPostfilter.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["cargo_guppy"] = [{"text":"impl&lt;'g&gt; <a class=\"trait\" href=\"guppy/graph/cargo/trait.CargoPostfilter.html\" title=\"trait guppy::graph::cargo::CargoPostfilter\">CargoPostfilter</a>&lt;'g&gt; for <a class=\"enum\" href=\"cargo_guppy/enum.Kind.html\" title=\"enum cargo_guppy::Kind\">Kind</a>","synthetic":false,"types":["cargo_guppy::core::Kind"]}];
+implementors["guppy"] = [];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
