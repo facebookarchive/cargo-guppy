16ccf985377f7d01c6e3b8f1cae604b6cb58b386
ci-build <ci-build@calibra.com>
2020-06-14 02:28:24 +0000

    [skip ci] documentation update

---
+++ target_spec/summaries/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["TargetFeaturesSummary","An owned, serializable version of `TargetFeatures`."]],"struct":[["PlatformSummary","An owned, serializable version of `Platform`."]]});
