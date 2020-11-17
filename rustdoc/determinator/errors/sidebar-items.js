325f5935b7ee61a243a2b31a14dd3a6257423d07
sunshowers <rain@sunshowers.io>
2020-11-17 06:56:18 +0000

    Deploying to gh-pages from  @ 5c91ad4e2cdc3fcd7b13e02bc03ece1ba86ae5b0 🚀

--- rustdoc/determinator/errors/sidebar-items.js
+++ rustdoc/determinator/errors/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["RuleIndex","The index of a rule."],["RulesErrorKind","The kind of error that occurred while parsing a set of determinator rules."]],"struct":[["RulesError","An error that occurred while resolving a set of determinator rules."]]});
+initSidebarItems({"enum":[["RulesErrorKind","The kind of error that occurred while parsing a set of determinator rules."]],"struct":[["RulesError","An error that occurred while resolving a set of determinator rules."]]});
