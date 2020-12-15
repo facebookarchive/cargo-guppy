86e1bbf058b6371e614faee80ee8931d123529aa
Rain <rain1@fb.com>
2020-12-14 17:53:32 -0800

    [guppy] turn Obs into a wrapper over Supercow
    Turn it from a struct into a trait.
    
    This is a breaking change, but not a major one.

--- guppy/src/obs.rs
+++ guppy/src/obs.rs
@@ -1,40 +1,37 @@
 // Copyright (c) The cargo-guppy Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use std::ops::Deref;
 use std::sync::Arc;
 use supercow::Supercow;
 
-mod private {
-    pub trait Sealed {}
-}
-
-/// Trait representing an owned, borrowed or shared instance of `T`.
+/// Represents an owned, borrowed or shared instance of `T`.
 ///
-/// This trait is implemented by `T`, `&'a T` and `Arc<T>`.
-pub trait Obs<'a, T>: private::Sealed {
-    #[doc(hidden)]
-    fn into_supercow(self) -> Supercow<'a, T>;
-}
+/// This represents any of `T`, `&'a T` or `Arc<T>`.
+#[derive(Clone, Debug)]
+pub struct Obs<'a, T>(Supercow<'a, T>);
 
-impl<T> private::Sealed for T {}
-
-impl<'a, T: Send> Obs<'a, T> for T {
-    #[doc(hidden)]
-    fn into_supercow(self) -> Supercow<'a, T> {
-        self.into()
+impl<'a, T> Deref for Obs<'a, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        self.0.deref()
     }
 }
 
-impl<'a, T: Sync> Obs<'a, T> for &'a T {
-    #[doc(hidden)]
-    fn into_supercow(self) -> Supercow<'a, T> {
-        self.into()
+impl<'a, T: Send> From<T> for Obs<'a, T> {
+    fn from(t: T) -> Obs<'a, T> {
+        Obs(t.into())
     }
 }
 
-impl<'a, T: 'static + Send + Sync> Obs<'a, T> for Arc<T> {
-    #[doc(hidden)]
-    fn into_supercow(self) -> Supercow<'a, T> {
-        self.into()
+impl<'a, T: Sync> From<&'a T> for Obs<'a, T> {
+    fn from(t: &'a T) -> Obs<'a, T> {
+        Obs(t.into())
+    }
+}
+
+impl<'a, T: 'static + Send + Sync> From<Arc<T>> for Obs<'a, T> {
+    fn from(t: Arc<T>) -> Obs<'a, T> {
+        Obs(t.into())
     }
 }
