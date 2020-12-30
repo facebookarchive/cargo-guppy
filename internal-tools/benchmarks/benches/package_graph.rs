4ecfbef8e10d4097cfe16c174b15f76b1d5aade5
Rain <rain1@fb.com>
2020-12-30 15:36:52 -0800

    [guppy] implement lookups by name and summary id
    Use the workspace path index for workspace packages, and a linear search
    for other packages. Should be fine based on measured performance, and
    can be optimized later.

--- internal-tools/benchmarks/benches/package_graph.rs
+++ internal-tools/benchmarks/benches/package_graph.rs
@@ -1,20 +1,21 @@
 // Copyright (c) The cargo-guppy Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 use guppy::{
-    graph::{DependencyDirection, PackageGraph},
+    graph::{DependencyDirection, PackageGraph, PackageMetadata},
     PackageId,
 };
 use proptest::{collection::vec, prelude::*};
 use proptest_ext::ValueGenerator;
+use std::{collections::HashMap, time::Instant};
 
 pub fn construct_benchmarks(c: &mut Criterion) {
     c.bench_function("make_package_graph", |b| b.iter(|| make_package_graph()));
 }
 
 pub fn query_benchmarks(c: &mut Criterion) {
-    let package_graph = make_package_graph();
+    let mut package_graph = make_package_graph();
     let mut cache = package_graph.new_depends_cache();
     let mut gen = ValueGenerator::deterministic();
 
@@ -58,11 +59,50 @@ pub fn query_benchmarks(c: &mut Criterion) {
             BatchSize::SmallInput,
         )
     });
+
+    c.bench_function("resolve_package_name", |b| {
+        b.iter_custom(|iters| {
+            package_graph.invalidate_caches();
+            let start = Instant::now();
+            for _ in 0..iters {
+                black_box({
+                    let package_set = package_graph.resolve_package_name("syn");
+                    assert_eq!(package_set.len(), 2, "2 versions of syn");
+                });
+            }
+            start.elapsed()
+        })
+    });
+
+    c.bench_function("make_package_name_hashmap", |b| {
+        b.iter_with_large_drop(|| {
+            let hashmap = make_package_name_hashmap(&package_graph);
+            assert_eq!(
+                hashmap.get("syn").map(|v| v.len()),
+                Some(2),
+                "2 versions of syn"
+            );
+        })
+    });
 }
 
 fn make_package_graph() -> PackageGraph {
     // Use this package graph as a large and representative one.
-    PackageGraph::from_json(include_str!("../../../fixtures/large/metadata_libra.json")).unwrap()
+    PackageGraph::from_json(include_str!(
+        "../../../fixtures/large/metadata_libra_9ffd93b.json"
+    ))
+    .unwrap()
+}
+
+fn make_package_name_hashmap<'g>(
+    graph: &'g PackageGraph,
+) -> HashMap<&'g str, Vec<PackageMetadata<'g>>> {
+    let mut hashmap: HashMap<&'g str, Vec<_>> = HashMap::new();
+    for package in graph.packages() {
+        hashmap.entry(package.name()).or_default().push(package);
+    }
+
+    hashmap
 }
 
 /// Generate pairs of IDs for benchmarks.
