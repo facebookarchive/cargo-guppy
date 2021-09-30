48d842667cc5f0c13794342374792d2f5c91a94f
Rain <rain1@fb.com>
2021-09-30 14:27:50 -0700

    [hakari] update documentation

--- tools/cargo-hakari/README.md
+++ tools/cargo-hakari/README.md
@@ -2,39 +2,109 @@
 
 [![cargo-hakari on crates.io](https://img.shields.io/crates/v/cargo-hakari)](https://crates.io/crates/cargo-hakari) [![Documentation (latest release)](https://docs.rs/cargo-hakari/badge.svg)](https://docs.rs/cargo-hakari/) [![Documentation (main)](https://img.shields.io/badge/docs-main-brightgreen)](https://facebookincubator.github.io/cargo-guppy/rustdoc/cargo-hakari/) [![License](https://img.shields.io/badge/license-Apache-green.svg)](../../LICENSE-APACHE) [![License](https://img.shields.io/badge/license-MIT-green.svg)](../../LICENSE-MIT)
 
-Manage workspace-hack crates.
+`cargo hakari` is a command-line application to workspace-hack crates.
+
+For an explanation of what workspace-hack packages are and how they can help, see the
+[`about` module](https://docs.rs/cargo-hakari/*/cargo_hakari/about).
+
+## Platform support
+
+* **Unix platforms**: Hakari works and is supported.
+* **Windows**: Hakari works and outputs file paths with forward slashes for
+  consistency with Unix. CRLF line endings are not supported in the workspace-hack's
+  `Cargo.toml` -- it is recommended that repositories disable automatic line ending conversion.
+  [Here's how to do it in Git](https://stackoverflow.com/a/10017566).
+  (Pull requests to improve this are welcome.)
+
+## Installation and usage
+
+All of the below commands take options that control their behavior.
+
+To install, run:
+
+```sh
+cargo install cargo-hakari
+```
+
+To update, run:
+
+```sh
+cargo install --force cargo-hakari
+```
+
+If `$HOME/.cargo/bin` is in your `PATH`, the `cargo hakari` command will be available.
+
+### Usage
+
+Initialize a workspace-hack crate for a workspace at path `my-workspace-hack`:
+
+```sh
+cargo hakari init my-workspace-hack
+```
+
+Generate or update the contents of a workspace-hack crate.
+
+```sh
+cargo hakari generate
+```
+
+Add the workspace-hack crate as a dependency to all other workspace crates:
+
+```sh
+cargo hakari manage-deps
+```
+
+Publish a crate that currently depends on the workspace-hack crate (`cargo publish` can't be
+used in this circumstance):
+
+```sh
+cargo hakari publish -p <crate>
+```
+
+### Disabling and uninstalling
+
+Disable the workspace-hack crate temporarily by removing generated contents. (Re-enable by
+running `cargo hakari generate`).
+
+```sh
+cargo hakari disable
+```
+
+Remove the workspace-hack crate as a dependency from all other workspace crates:
+
+```sh
+cargo hakari remove-deps
+```
 
 ## Configuration
 
-`cargo-hakari` is configured through the `.guppy/hakari.toml` file at the root of the workspace.
+`cargo hakari` is configured through `.guppy/hakari.toml` at the root of the workspace.
 
 Example configuration:
 
 ```toml
-# Which version of the Cargo resolver to use. This should match the "resolver" key in
-# `Cargo.toml`.
-resolver = "2"
-# The name of the package used for workspace-hack unification.
+## The name of the package used for workspace-hack unification.
 hakari-package = "workspace-hack"
+## Cargo resolver version in use -- version 2 is highly recommended.
+resolver = "2"
 
-# The platforms to perform resolution on. Can be left blank to perform a unified resolution
-# across all platforms. (This may lead to potentially unexpected results.)
+## Add triples corresponding to platforms commonly used by developers here.
+## https://doc.rust-lang.org/rustc/platform-support.html
 platforms = [
-    { triple = "x86_64-unknown-linux-gnu", target-features = "unknown" }
+    ## "x86_64-unknown-linux-gnu",
+    ## "x86_64-apple-darwin",
+    ## "x86_64-pc-windows-msvc",
 ]
 
-# Options to control Hakari output.
+## Options to control Hakari output.
 [output]
-# Write out exact versions rather than specifications. Set this to true if version numbers in
-# `Cargo.toml` and `Cargo.lock` files are kept in sync, e.g. in some configurations of
-# https://dependabot.com/.
-# exact-versions = false
-
-# Write out a summary of builder options as a comment in the workspace-hack Cargo.toml.
-# builder-summary = false
+## Write out exact versions rather than specifications. Set this to true if version numbers in
+## `Cargo.toml` and `Cargo.lock` files are kept in sync, e.g. in some configurations of
+## https://dependabot.com/.
+## exact-versions = false
 ```
 
-For more options, see the (TODO XXX configuration.md).
+For more options, see the [`config` module](https://docs.rs/cargo-hakari/*/cargo_hakari/config).
 
 ## Contributing
 
--- tools/hakari/src/lib.rs
+++ tools/hakari/src/lib.rs
@@ -3,7 +3,8 @@
 
 #![warn(missing_docs)]
 
-//! `hakari` is a set of tools to manage `workspace-hack` packages.
+//! `hakari` is the library underlying [`cargo hakari`](https://docs.rs/cargo-hakari/*), a tool to
+//! manage `workspace-hack` packages.
 //!
 //! # Examples
 //!
@@ -34,98 +35,6 @@
 //!
 //! The `cargo-guppy` repository also has a number of fixtures that demonstrate Hakari's output.
 //! [Here is an example](https://github.com/facebookincubator/cargo-guppy/blob/main/fixtures/guppy/hakari/metadata_guppy_869476c-1.toml).
-//!
-//! # Platform support
-//!
-//! * **Unix platforms**: Hakari works and is supported.
-//! * **Windows**: Hakari works and outputs file paths with forward slashes for
-//!   consistency with Unix. CRLF line endings are not supported in the workspace-hack's
-//!   `Cargo.toml` -- it is recommended that repositories disable automatic line ending conversion.
-//!   [Here's how to do it in Git](https://stackoverflow.com/a/10017566).
-//!   (Pull requests to improve this are welcome.)
-//!
-//! # What are workspace-hack packages?
-//!
-//! Let's say you have a Rust crate `my-crate` with two dependencies:
-//!
-//! ```toml
-//! # my-crate/Cargo.toml
-//! [dependencies]
-//! foo = "1.0"
-//! bar = "2.0"
-//! ```
-//!
-//! Let's say that `foo` and `bar` both depend on `baz`:
-//!
-//! ```toml
-//! # foo-1.0/Cargo.toml
-//! [dependencies]
-//! baz = { version = "1", features = ["a", "b"] }
-//!
-//! # bar-2.0/Cargo.toml
-//! [dependencies]
-//! baz = { version = "1", features = ["b", "c"] }
-//! ```
-//!
-//! What features is `baz` built with?
-//!
-//! One way to resolve this question might be to build `baz` twice with each requested set of
-//! features. But this is likely to cause a combinatorial explosion of crates to build, so Cargo
-//! doesn't do that. Instead,
-//! [Cargo builds `baz` once](https://doc.rust-lang.org/nightly/cargo/reference/features.html?highlight=feature#feature-unification)
-//! with the *union* of the features enabled for the package: `[a, b, c]`.
-//!
-//! ---
-//!
-//! **NOTE:** This description elides some details around unifying build and dev-dependencies: for
-//! more about this, see the documentation for guppy's
-//! [`CargoResolverVersion`](guppy::graph::cargo::CargoResolverVersion).
-//!
-//! ---
-//!
-//! Now let's say you're in a workspace, with a second crate `your-crate`:
-//!
-//! ```toml
-//! # your-crate/Cargo.toml
-//! [dependencies]
-//! baz = { version = "1", features = ["c", "d"] }
-//! ```
-//!
-//! In this situation:
-//!
-//! | if you build                                 | `baz` is built with |
-//! | -------------------------------------------- | ------------------- |
-//! | just `my-crate`                              | `a, b, c`           |
-//! | just `your-crate`                            | `c, d`              |
-//! | `my-crate` and `your-crate` at the same time | `a, b, c, d`        |
-//!
-//! Even in this simplified scenario, there are three separate ways to build `baz`. For a dependency
-//! like [`syn`](https://crates.io/crates/syn) that have
-//! [many optional features](https://github.com/dtolnay/syn#optional-features),
-//! large workspaces end up with a very large number of possible build configurations.
-//!
-//! Even worse, the feature set of a package affects everything that depends on it, so `syn`
-//! being built with a slightly different feature set than before would cause *every package that
-//! directly or transitively depends on `syn` to be rebuilt. For large workspaces, this can result
-//! a lot of wasted build time.
-//!
-//! ---
-//!
-//! To avoid this problem, many large workspaces contain a `workspace-hack` package. The
-//! purpose of this package is to ensure that dependencies like `syn` are always built with the same
-//! feature set no matter which workspace packages are currently being built. This is done by:
-//! 1. adding dependencies like `syn` to `workspace-hack` with the full feature set required by any
-//!   package in the workspace
-//! 2. adding `workspace-hack` as a dependency of every crate in the repository.
-//!
-//! Some examples of `workspace-hack` packages:
-//!
-//! * Rust's [`rustc-workspace-hack`](https://github.com/rust-lang/rust/blob/0bfc45aa859b94cedeffcbd949f9aaad9f3ac8d8/src/tools/rustc-workspace-hack/Cargo.toml)
-//! * Firefox's [`mozilla-central-workspace-hack`](https://hg.mozilla.org/mozilla-central/file/cf6956a5ec8e21896736f96237b1476c9d0aaf45/build/workspace-hack/Cargo.toml)
-//! * Diem's [`diem-workspace-hack`](https://github.com/diem/diem/blob/91578fec8d575294b47b3ee7af691fd9dc6eb240/common/workspace-hack/Cargo.toml)
-//!
-//! These packages have historically been maintained by hand, on a best-effort basis. `hakari` is an
-//! attempt to automate the maintenance of these packages.
 //!
 //! # How `hakari` works
 //!
