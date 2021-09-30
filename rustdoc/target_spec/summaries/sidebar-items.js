41dc4c12b7ebd3741780b9a70a441f84a40a3a74
sunshowers <rain@sunshowers.io>
2021-09-30 21:03:44 +0000

    Deploying to gh-pages from  @ 82bb2739ad90899af8712110dcafa66b511265f3 🚀

--- rustdoc/target_spec/summaries/sidebar-items.js
+++ rustdoc/target_spec/summaries/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["TargetFeaturesSummary","An owned, serializable version of `TargetFeatures`."]],"struct":[["PlatformSummary","An owned, serializable version of `Platform`."]]});
+initSidebarItems({"enum":[["TargetFeaturesSummary","An owned, serializable version of [`TargetFeatures`]."]],"struct":[["PlatformSummary","An owned, serializable version of [`Platform`]."]]});
