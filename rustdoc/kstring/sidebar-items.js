36149cb62d5c098cfcd4332dccf972017893ecaa
sunshowers <rain@sunshowers.io>
2021-11-23 19:09:54 +0000

    Deploying to gh-pages from  @ feb94e560734b595c482054c4ebfa9766d3737c0 🚀

---
+++ rustdoc/kstring/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["KString","A UTF-8 encoded, immutable string."],["KStringCow","A reference to a UTF-8 encoded, immutable string."],["KStringRef","A reference to a UTF-8 encoded, immutable string."]]});
