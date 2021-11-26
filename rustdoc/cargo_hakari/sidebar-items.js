828c949c0d9fabcdc51d91baffb797f02d5c6c80
sunshowers <rain@sunshowers.io>
2021-11-26 01:12:50 +0000

    Deploying to gh-pages from  @ 6de10abaca6272659c8e8152d67f9e6232084f08 🚀

--- rustdoc/cargo_hakari/sidebar-items.js
+++ rustdoc/cargo_hakari/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["about","About workspace-hack crates, how `cargo hakari` manages them, and how much faster they make builds."],["config","Configuration for `cargo hakari`."]]});
+initSidebarItems({"mod":[["about","About workspace-hack crates, how `cargo hakari` manages them, and how much faster they make builds."],["config","Configuration for `cargo hakari`."],["publishing","Publishing a package to `crates.io` or other registries."]]});
