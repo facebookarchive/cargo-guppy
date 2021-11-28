fcbf90fc68cdcf84fdc6edaf2dc2adc2ff7f2b7b
Rain <rain1@fb.com>
2021-11-28 13:19:00 -0800

    [hakari] rename guppy-workspace-hack to just workspace-hack
    Going to turn this crate into a reusable target for any downstream
    consumers.

--- tools/hakari/src/lib.rs
+++ tools/hakari/src/lib.rs
@@ -17,9 +17,8 @@
 //!     .build_graph()
 //!     .expect("obtained cargo-guppy's PackageGraph");
 //!
-//! // The second argument to HakariBuilder::new specifies a Hakari (workspace-hack) package. For
-//! // the cargo-guppy repository it is called "guppy-workspace-hack".
-//! let hakari_package = package_graph.workspace().member_by_name("guppy-workspace-hack").unwrap().id();
+//! // The second argument to HakariBuilder::new specifies a Hakari (workspace-hack) package.
+//! let hakari_package = package_graph.workspace().member_by_name("workspace-hack").unwrap().id();
 //! let hakari_builder = HakariBuilder::new(&package_graph, Some(hakari_package))
 //!     .expect("HakariBuilder was constructed");
 //!
---
+++ workspace-hack/README.md
@@ -0,0 +1,9 @@
+# workspace-hack
+
+This is a stub crate that will never contain any code or have any dependencies.
+
+It is intended for the [`cargo hakari`](https://crates.io/crates/cargo-hakari) workspace-hack manager, but
+anyone is welcome to use it for any purpose.
+
+For more about why this crate has been published, see the
+[`cargo hakari` publishing docs](https://docs.rs/cargo-hakari/*/cargo_hakari/publishing).
