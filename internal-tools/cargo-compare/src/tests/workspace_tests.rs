ffcdd553862e326e77c15fef6c9c163eecc07773
Rain <rain1@fb.com>
2022-02-02 15:35:20 -0800

    [guppy] handle inactive, optional dependencies better
    There was a slight issue with the way we were handling inactive,
    optional dependencies. This PR should fix that.
    
    Also expand the inside-outside tests to cover both the v1 and v2
    resolvers; requires copying the fixture into a temp dir so that the
    'resolver = "1"' or "2" can be added to it.
    
    hakari output changes slightly -- this is a bugfix so it's a
    non-breaking change.
    
    See the comments in this PR for more.

--- internal-tools/cargo-compare/src/tests/workspace_tests.rs
+++ internal-tools/cargo-compare/src/tests/workspace_tests.rs
@@ -1,5 +1,6 @@
 // Copyright (c) The cargo-guppy Contributors
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
-proptest_suite!(inside_outside);
+proptest_suite!(inside_outside_v1);
+proptest_suite!(inside_outside_v2);
 proptest_suite!(cargo_guppy);
